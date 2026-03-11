86d1dd7c9576e476aa862504c2148644a1e20381
Currypan1229 <67533379+Currypan1229@users.noreply.github.com>
2026-03-12 04:36:57 +0900

    Refactor database config interfaces and update implementation
    Replaced `DBConfig` with `IDatabaseConfigData` for more modular configuration handling.
    Added setter methods in `IDatabaseConfigData` to support more flexible updates.
    Updated `DatabaseConfigData` to implement the new interface.

--- doma/impl/src/main/java/org/akazukin/plugin/repo/doma/config/DatabaseConfigData.java
+++ doma/impl/src/main/java/org/akazukin/plugin/repo/doma/config/DatabaseConfigData.java
@@ -4,13 +4,12 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-import org.akazukin.plugin.config.config.data.IConfigData;
 import org.akazukin.plugin.repo.doma.driver.IDriver;
 
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public final class DatabaseConfigData implements IConfigData {
+public final class DatabaseConfigData implements IDatabaseConfigData {
     IDriver driver;
 
     String authority;
